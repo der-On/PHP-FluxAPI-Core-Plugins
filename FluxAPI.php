62df87f073b5e2034c3dd3cae0ff9bfa4d806e47
Ondrej Brinkel <info@anzui.de>
2013-05-28 11:34:25 +0200

    ModelEvents comming from FluxAPI plugin can now be disabled using 'FluxAPI/ModelEvents'

--- FluxAPI.php
+++ FluxAPI.php
@@ -31,23 +31,25 @@ class FluxAPI extends \FluxAPI\Plugin
 
     protected static function _registerModelEvents(\FluxAPI\Api $api)
     {
-        // register listeners for models to update author, updatedAt and createdAt
-        $api->on(ModelEvent::CREATE, function(ModelEvent $event) {
-            $model = $event->getModel();
+        if (!in_array('FluxAPI/ModelEvents', $api->config['plugin.options']['disabled'])) {
+            // register listeners for models to update author, updatedAt and createdAt
+            $api->on(ModelEvent::CREATE, function(ModelEvent $event) {
+                $model = $event->getModel();
 
-            if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model') && $model->isNew()) {
-                $now = new \DateTime();
-                $model->createdAt = $now;
-            }
-        }, \FluxAPI\Api::EARLY_EVENT);
+                if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model') && $model->isNew()) {
+                    $now = new \DateTime();
+                    $model->createdAt = $now;
+                }
+            }, \FluxAPI\Api::EARLY_EVENT);
 
-        $api->on(ModelEvent::BEFORE_SAVE, function(ModelEvent $event) {
-            $model = $event->getModel();
+            $api->on(ModelEvent::BEFORE_SAVE, function(ModelEvent $event) {
+                $model = $event->getModel();
 
-            if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model')) {
-                $now = new \DateTime();
-                $model->updatedAt = $now;
-            }
-        }, \FluxAPI\Api::EARLY_EVENT);
+                if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model')) {
+                    $now = new \DateTime();
+                    $model->updatedAt = $now;
+                }
+            }, \FluxAPI\Api::EARLY_EVENT);
+        }
     }
 }
