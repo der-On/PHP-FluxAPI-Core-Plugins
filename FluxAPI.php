092ce2b29ecff784374b69361d82c8315fa3e53a
Ondrej Brinkel <info@anzui.de>
2013-05-30 11:23:10 +0200

    - moved ModelEvents into its own class - added BEFORE_UPDATE event handler to model events that will update the updatedAt field

--- FluxAPI.php
+++ FluxAPI.php
@@ -1,8 +1,6 @@
 <?php
 namespace Plugins\FluxAPI;
 
-use \FluxAPI\Event\ModelEvent;
-
 class FluxAPI extends \FluxAPI\Plugin
 {
     public static function register(\FluxAPI\Api $api)
@@ -32,24 +30,7 @@ class FluxAPI extends \FluxAPI\Plugin
     protected static function _registerModelEvents(\FluxAPI\Api $api)
     {
         if (!in_array('FluxAPI/ModelEvents', $api->config['plugin.options']['disabled'])) {
-            // register listeners for models to update author, updatedAt and createdAt
-            $api->on(ModelEvent::CREATE, function(ModelEvent $event) {
-                $model = $event->getModel();
-
-                if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model') && $model->isNew()) {
-                    $now = new \DateTime();
-                    $model->createdAt = $now;
-                }
-            }, \FluxAPI\Api::EARLY_EVENT);
-
-            $api->on(ModelEvent::BEFORE_SAVE, function(ModelEvent $event) {
-                $model = $event->getModel();
-
-                if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model')) {
-                    $now = new \DateTime();
-                    $model->updatedAt = $now;
-                }
-            }, \FluxAPI\Api::EARLY_EVENT);
+            ModelEvents::register($api);
         }
     }
 }
---
+++ ModelEvents.php
@@ -0,0 +1,43 @@
+<?php
+namespace Plugins\FluxAPI;
+
+use \FluxAPI\Event\ModelEvent;
+
+class ModelEvents
+{
+    public static function register(\FluxAPI\Api $api)
+    {
+        // register listeners for models to update author, updatedAt and createdAt
+        $api->on(ModelEvent::CREATE, function(ModelEvent $event) {
+            $model = $event->getModel();
+
+            if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model') && $model->isNew()) {
+                $now = new \DateTime();
+                $model->createdAt = $now;
+            }
+        }, \FluxAPI\Api::EARLY_EVENT);
+
+        $api->on(ModelEvent::BEFORE_SAVE, function(ModelEvent $event) {
+            $model = $event->getModel();
+
+            if (!empty($model) && is_subclass_of($model, '\\Plugins\\FluxAPI\\Model')) {
+                $now = new \DateTime();
+                $model->updatedAt = $now;
+            }
+        }, \FluxAPI\Api::EARLY_EVENT);
+
+        $api->on(ModelEvent::BEFORE_UPDATE, function(ModelEvent $event) use ($api) {
+            $model_name = $event->getModelName();
+
+            $model_class = $api['plugins']->getPluginClass('Model', $model_name);
+
+            if ($model_class) {
+                $model = new $model_class($api);
+
+                if (is_subclass_of($model, '\\Plugins\\FluxAPI\\Model')) {
+                    $event->getQuery()->setDataField('updatedAt', new \DateTime());
+                }
+            }
+        }, \FluxAPI\Api::EARLY_EVENT);
+    }
+}
